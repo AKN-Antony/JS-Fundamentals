e788db652a4f2c73e5d3604512c0cfcec3e3d9f6
AKN-Antony <kamauanthony345@gmail.com>
2025-07-13 21:22:48 +0300

    Add 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,4 +4,8 @@ const count = parseInt(process.argv[2]);
 if (isNaN(count)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i
+  console.log(`Correct output - ${count} times`);
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+}
